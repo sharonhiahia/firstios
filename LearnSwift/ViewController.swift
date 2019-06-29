e58853670af83a4b1afb77d30a1b1e4de4ddcb52
RongX <sharoneaecs@gmail.com>
2019-06-28 17:00:00 -0700

    Initial Commit

---
+++ LearnSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnSwift
+//
+//  Created by Rong Xiao on 6/28/19.
+//  Copyright © 2019 Rong Xiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
